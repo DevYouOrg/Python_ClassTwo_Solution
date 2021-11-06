93f4098ff016047eac1074992dcfa2dd7c3c15fe
Hemory Phifer <hphife@yahoo.com>
2021-11-05 23:14:35 -0400

    solution

---
+++ main.py
@@ -0,0 +1,37 @@
+# Exercise One
+birth_month = input("Enter your birth month: ")
+print(birth_month)
+
+# Challenge
+user_name = input("Enter name: ")
+print(f"{user_name}, your birth month is {birth_month}.")
+
+# Exercise Two
+num1 = int(input("Enter num: "))
+num2 = int(input("Enter num: "))
+
+print(f"{num1} + {num2} = {num1+num2}")
+
+# Exercise Three
+number1 = 5
+number2 = 3
+
+print(f"{number1} - {number2} = {number1-number2}")
+print(f"{number1} * {number2} = {number1*number2}")
+print(f"{number1} / {number2} = {number1/number2}")
+print(f"{number1} % {number2} = {number1%number2}")
+
+# Challenge
+
+num1 = float(input("Enter num: "))
+num2 = float(input("Enter num: "))
+
+addition = f"{num1} + {num2} = {num1+num2}"
+difference = f"{num1} - {num2} = {num1-num2}"
+product = f"{num1} * {num2} = {num1*num2}"
+quotient = f"{num1} / {num2} = {num1/num2}"
+
+print(addition)
+print(difference)
+print(product)
+print(quotient)
